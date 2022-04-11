7c3509a8cbb9c6ad5e32a75a8088035603e422ca
vityah1 <vholoshivskiy@gmail.com>
2022-04-11 16:42:30 +0300

    edit user

--- test_create_user.py
+++ test_create_user.py
@@ -7,6 +7,13 @@ from app import app
 
 test_user = f"user_2"
 
+item_id = 1
+accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0OTY4NDIwNiwianRpIjoiNzY0NmQxYTMtM2NlOC00MGJiLTlmMDItOTJhZjkzZjIyMWZlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjQ5Njg0MjA2LCJleHAiOjE2NTIyNzYyMDZ9.rq1Wz9yHa8CefIPbVh_oUI4hegNFXGla_0qXvJZELm8"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {accessToken}",
+}
+
 
 @pytest.fixture
 def client():
@@ -48,6 +55,18 @@ def login_user(client, test_user):
     return resp.json.get("accessToken", "")
 
 
+def edit_user(client, test_user):
+
+    resp = client.put(
+        "/api/auth/edit",
+        json={"username": "new_" + test_user, "avatar": "avatar_" + test_user},
+        headers=headers,
+    )
+    assert 200 == resp.status_code
+    assert f"User {'new_' + test_user} edited OK" == resp.json["message"]
+    assert "ok" == resp.json["status"]
+
+
 def test_create_user(client):
 
     create_user(client, test_user)
@@ -56,3 +75,8 @@ def test_create_user(client):
 def test_login_user(client):
 
     login_user(client, test_user)
+
+
+def test_edit_user(client):
+
+    edit_user(client, test_user)
