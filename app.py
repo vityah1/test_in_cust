a7c4da1a64d67f5dec98f04a01d955b3a1e7dfec
vityah1 <vholoshivskiy@gmail.com>
2022-04-12 10:23:46 +0300

    port 80

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ if __name__ == "__main__":
     #    app.run(debug=True)
     #    app.debug=True
     #    app.run(host='0.0.0.0',port=4000)
-    #    app.run(host='0.0.0.0',port=80,debug=False)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
